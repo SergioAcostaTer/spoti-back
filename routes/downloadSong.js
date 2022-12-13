545b3d8c80ce8eff0d19335e6208aa7e8bf674ad
Sergio Acosta <sergioacostaquintana@gmail.com>
2022-12-13 19:14:32 +0000

    try catch

--- routes/downloadSong.js
+++ routes/downloadSong.js
@@ -5,16 +5,20 @@ const { searchYoutube } = require("../services/searchYoutube.js");
 const ytdl = require("ytdl-core");
 
 router.get("/download/:name/:artist/:time", async (req, res) => {
-  const { name, artist, time } = req.params;
+  try {
+    const { name, artist, time } = req.params;
 
-  const searched = await searchYoutube({ name, artist, time });
+    const searched = await searchYoutube({ name, artist, time });
 
-  res.header(
-    "Content-Disposition",
-    `attachment; filename="${name} - ${artist}.mp3`
-  );
-  console.log(name, artist)
-  ytdl(searched.link, { format: "mp3", quality: "lowestaudio" }).pipe(res);
+    res.header(
+      "Content-Disposition",
+      `attachment; filename="${name} - ${artist}.mp3`
+    );
+    console.log(name, artist);
+    ytdl(searched.link, { format: "mp3", quality: "lowestaudio" }).pipe(res);
+  } catch (error) {
+    console.log(error)
+  }
 });
 
 module.exports = router;
